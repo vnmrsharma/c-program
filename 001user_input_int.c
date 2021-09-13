182c1679e29803b96408ae7249665f1a7510ddbc
Savithry <66897282+savithryj@users.noreply.github.com>
2021-09-13 22:53:25 +0530

    Create 001user_input_int.c

---
+++ 001user_input_int.c
@@ -0,0 +1,15 @@
+// Take Integer input from user and print it
+
+#include <stdio.h>
+
+int main(){
+
+    // if input is an integer
+    int num;
+
+    printf("Enter an integer as input: \n");
+    scanf("%d", &num);
+    printf("Entered integer is %d \n", num);
+
+    return 0;
+}
