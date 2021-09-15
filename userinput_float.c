c69f89cd491d466afdbf9d8628e5ebb4a89b5203
Savithry <66897282+savithryj@users.noreply.github.com>
2021-09-15 19:51:38 +0530

    Create userinput_float.c

---
+++ userinput_float.c
@@ -0,0 +1,14 @@
+// Take Input from user and print it
+
+#include <stdio.h>
+
+int main(){
+
+    //if user input is a float number
+    float num;
+    printf("Enter a float number: \n");
+    scanf("%f",&num);
+    printf("Entered float number is %f ", num);
+
+    return 0;
+}
