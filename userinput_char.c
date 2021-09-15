146166c2f936138a28013f838b2843b5b9ee4535
Savithry <66897282+savithryj@users.noreply.github.com>
2021-09-15 22:20:33 +0530

    Create userinput_char.c

---
+++ userinput_char.c
@@ -0,0 +1,15 @@
+// Take Input from user and print it
+
+#include <stdio.h>
+
+int main(){
+
+    // if user input is a character variable
+    char ch;
+
+    printf("Enter the character variable as input: \n");
+    scanf("%c", &ch);
+    printf("Entered input is %c",ch);
+
+    return 0;
+}
