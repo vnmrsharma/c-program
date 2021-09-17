5c721e19ccc20b2595d30fc1b493f5a0a0b5aac9
Savithry <66897282+savithryj@users.noreply.github.com>
2021-09-17 23:48:09 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,14 @@
+// Take Input from user and print it
+
+#include <stdio.h>
+
+int main(){
+
+    //if input is a word
+    char str[50];
+    printf("Enter a word:");
+    scanf("%s",str);
+    printf("Entered word is %s",str);
+
+    return 0;
+}
