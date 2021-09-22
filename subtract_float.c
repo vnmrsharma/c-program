f3aa24fdbd3188cd1b1ae95c8c64bf8b7a480fd5
Savithry <66897282+savithryj@users.noreply.github.com>
2021-09-22 20:04:13 +0530

    Create subtract_float.c

---
+++ subtract_float.c
@@ -0,0 +1,20 @@
+/* Take two input,
+   perform mathematical operations 
+   and print result*/
+
+#include<stdio.h>
+
+int main(){
+
+    //if input is float value
+    float num1, num2, result;
+    printf("Enter two float values: \n");
+    scanf("%f %f", &num1, &num2);
+    result = num1 - num2;
+    printf("%f - %f = %f \n", num1, num2, result);
+
+    //to limit the float value to two decimal places:
+    printf("%.2f - %.2f = %.2f \n", num1, num2, result);
+
+    return 0;
+}
